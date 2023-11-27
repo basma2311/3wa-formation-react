3defb1c64429f0177f771f8f12da212b5a38924e
basma <basmahadhri@outlook.fr>
2023-11-27 21:34:28 +0100

    exercice character accompli

--- src/App.js
+++ src/App.js
@@ -1,25 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
+import data from "./assets/data";
+import Caracter from "./Components/Caracter";
+import Carte from "./Components/Caracter";
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="min-h-screen bg-slate-700 font-mono ">
+      <ul className="grid sm:grid-cols-2 md:grid-cols-3 gap-4 justify-center items-star ">
+        {data.map((item, index) => (
+          <li key={index} >
+            <Caracter
+              frame={item.image}
+              name={item.name}
+              description={item.description}
+            />
+
+
+          </li>
+
+        ))}
+
+      </ul>
     </div>
-  );
-}
+  )
+};
 
 export default App;
+
---
+++ src/Components/Caracter.js
@@ -0,0 +1,19 @@
+import React from 'react'
+
+export default function Caracter(props) {
+  return (
+    <div className='bg-white max-w-xs rounded-xl'>
+          <img 
+          className='rounded-xl '
+          src={props.frame} />
+          <h2 className='m-3 text-xl font-extrabold'>
+            {props.name}</h2>
+          <p className='m-l-3 font-normal font-mono'>
+            {props.description}</p>
+          <button className='bg-blue-700 m-2 p-2 rounded-xl text-white'>
+
+            read more</button>
+
+    </div>
+  )
+}
